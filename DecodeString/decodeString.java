5165c0f6c03bb7f112ee6cdc151948a2723a83e0
Shivaraj Shetty <shivarajshetty635@gmail.com>
2024-08-13 08:28:53 +0530

    Added solution 2 using repeat method

--- DecodeString/decodeString.java
+++ DecodeString/decodeString.java
@@ -7,13 +7,25 @@ class decodeString{
         System.out.println("Enter the encoded string");
         String inp = sc.next();
         System.out.println(inp);
+        String output = "";
         
-        for(int i = 0;i<inp.length(); i+=2)
-        {
-            for(int j=inp.charAt(i+1)-'0'; j>0;j--)
-            {
-                System.out.print( inp.charAt(i));
-            }
+        //Solution 1 : Normal Approah
+        // for(int i = 0;i<inp.length(); i+=2)
+        // {
+        //     for(int j=inp.charAt(i+1)-'0'; j>0;j--)
+        //     {
+        //         System.out.print( inp.charAt(i));
+        //     }
+        // }
+
+        // Solution 2 using Repeat method
+         for (int i = 0; i < inp.length(); i += 2) 
+         {
+            char character = inp.charAt(i);
+            int count = inp.charAt(i + 1) - '0'; 
+            output += String.valueOf(character).repeat(count); 
         }
+
+        System.out.println(output);
     }
 }
